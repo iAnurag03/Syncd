4a68fe331ee80e50d54fc88408dfa037c7cf2008
iAnurag03 <anuragdadarya007@gmail.com>
2025-07-02 18:53:50 +0530

    project setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { Button } from './components/ui/button';
 
 export default function App() {
   return (
+    <>
     <header>
       <SignedOut>
-        <SignInButton />
+        <SignInButton>
+          <Button className="bg-emerald-700">
+            Sign In
+          </Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
     </header>
+    </>
+    
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -12,7 +13,10 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+       <App />
+    </ClerkProvider>
+   
   </StrictMode>,
 )
 
