66cb03b36881f279f8f7c3e554ff2eda519bb0be
iAnurag03 <anuragdadarya007@gmail.com>
2025-07-02 19:03:24 +0530

    backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import dotenv from "dotenv"
+import userRoutes from "../routes/user.routes.js"
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("api/users", userRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`server running at ${PORT}`)
+})
