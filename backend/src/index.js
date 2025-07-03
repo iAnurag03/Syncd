c28909e71a7dcdfae8c21e08545fac13c0e6f438
iAnurag03 <anuragdadarya007@gmail.com>
2025-07-03 11:41:24 +0530

    setting up database and define models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
-import userRoutes from "../routes/user.routes.js"
+import userRoutes from "./routes/user.routes.js"
+import { connectDB } from "./lib/db.js";
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT;
 
-app.use("api/users", userRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", userRoutes);
+app.use("/api/albums", userRoutes);
+app.use("/api/stats", userRoutes);
+
 
 app.listen(PORT,()=>{
-    console.log(`server running at ${PORT}`)
+    console.log(`server running at ${PORT}`);
+    connectDB();
 })
